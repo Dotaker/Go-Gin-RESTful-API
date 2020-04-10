77f57204cafbfeb7aea5cd123f77611cbfc1a799
Dotaker <49651680+Dotaker@users.noreply.github.com>
2020-04-10 21:57:45 +0200

    tests update, debug update

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -2,10 +2,9 @@ package routes_test
 
 import (
 	"CRUD-Operation/routes"
-	"bytes"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -26,26 +25,63 @@ type User struct {
 
 func TestGetAllUsersRoute(t *testing.T) {
 	router := routes.StartGin()
+	users := user.Users{}
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/api/users", nil)
 	router.ServeHTTP(w, req)
 
-	users := user.Users{}
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(t, gin.H{"status": "success", "users": &users}, gin.H{"status": "success", "users": &users})
 }
 
 func TestCreateUserRoute(t *testing.T) {
 	router := routes.StartGin()
+	user := user.User{}
 
 	w := httptest.NewRecorder()
-	reqBody, _ := json.Marshal(map[string]string{"Name": "Admin", "Address": "Home"})
-	req, _ := http.NewRequest("POST", "/api/users", bytes.NewBuffer(reqBody))
+	req, _ := http.NewRequest("POST", "/api/users", strings.NewReader(`{ "Name":"admin","Address":"Home"}`))
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	router.ServeHTTP(w, req)
 
-	user := user.User{}
 	assert.Equal(t, 201, w.Code)
 	assert.Equal(t, gin.H{"status": "success", "user": &user}, gin.H{"status": "success", "user": &user})
+}
+
+func TestGetUserRoute(t *testing.T) {
+	router := routes.StartGin()
+	user := user.User{}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/users/5e90a2c3b82d3752a8baae69", nil)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, gin.H{"status": "success", "user": &user}, gin.H{"status": "success", "user": &user})
+}
+
+func TestUpdateUserRoute(t *testing.T) {
+	router := routes.StartGin()
+	user := user.User{}
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/api/users/5e90a2c3b82d3752a8baae69", strings.NewReader(`{ "Name":"Quentin","Address":"Venelles"}`))
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, gin.H{"status": "success", "user": &user}, gin.H{"status": "success", "user": &user})
+}
+
+func TestDeleteUserRoute(t *testing.T) {
+	router := routes.StartGin()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/api/users/5e90c641b82d377a1e08b291", nil)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, gin.H{"status": "success", "message": "User deleted successfully"}, gin.H{"status": "success", "message": "User deleted successfully"})
 }
