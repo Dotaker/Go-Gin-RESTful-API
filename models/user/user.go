d21faeb17deeab2fd2f50d4f9e6c44fcc5179c02
Dotaker <49651680+Dotaker@users.noreply.github.com>
2020-04-10 12:15:50 +0200

    delete age in user model

--- models/user/user.go
+++ models/user/user.go
@@ -12,7 +12,6 @@ type User struct {
 	ID        bson.ObjectId `bson:"_id"`
 	Name      string        `bson:"name"`
 	Address   string        `bson:"address"`
-	Age       int           `bson:"age"`
 	CreatedAt time.Time     `bson:"created_at"`
 	UpdatedAt time.Time     `bson:"updated_at"`
 }
